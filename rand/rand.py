abfd0133a65538541e1e19de054a1ee7a366b6fc
bradpittsexyguy <97422500+bradpittsexyguy@users.noreply.github.com>
2022-01-10 06:55:36 +0100

    Update rand.py

--- rand/rand.py
+++ rand/rand.py
@@ -19,5 +19,23 @@ class Rand(commands.Cog):
         else:
             message = text
         await ctx.send(message)
+
+    @commands.command(name="react")
+    @commands.cooldown(rate=1, per+3, type=commands.BucketType.user)
+    async def reaction(self, ctx: commands.Context, msg_id: int, emoji = None, channel: discord.TextChannel = None):
+        """react to a message.\n
+        msg_id is the message id you want to react to.\n
+        emoji is a emoji.\n
+        channel is a channel"""
+        if not channel:
+            channel  = ctx.channel
+        if not emoji:
+            emoji = "✅"
+        try:
+            msg = await channel.fetch_message(msg_id)
+            await msg.add_reaction(emoji)
+        except discord.HTTPException:
+            await ctx.send("that message id isn't in this channel please specify the channel the message is in or use the correct message id.")
+
     
     
